b4fc066219baba42c52781c2ed1e1183be86f879
hung <bahung1tt@gmail.com>
2022-06-29 17:15:53 +0700

    Restructure files
    Data mananger to load IEEE-CIS.
    setup.py to turn into package.

---
+++ dataset/ieee-cis/data_manager.py
@@ -0,0 +1,34 @@
+from tsf_loader import convert_tsf_to_dataframe
+import datetime
+
+class DataManager:
+  def __init__(self, tsf_file_path):
+    tsf = convert_tsf_to_dataframe(path)
+
+    self.df = tsf[0]
+
+    series_name = self.df["series_name"]
+    series_value = self.df["series_value"]
+    start_timestamp = self.df["start_timestamp"]
+
+    phase_1_time = datetime.datetime(day=1, month=10, year=2020, hour=0, minute=0, second=0)
+    phase_2_time = datetime.datetime(day=1, month=11, year=2020, hour=0, minute=0, second=0)
+
+    self.train = dict()
+    self.phase1 = dict()
+    self.phase2 = dict()
+
+    for name, value, stime in zip(series_name, series_value, start_timestamp):
+      phase_1_idx = int((phase_1_time - stime).total_seconds()/60/15)
+      phase_2_idx = int((phase_2_time - stime).total_seconds()/60/15)
+
+      self.train[name] = value[:phase_1_idx]
+      self.phase1[name] = value[phase_1_idx:phase_2_idx]
+      self.phase2[name] = value[phase_2_idx:]
+
+
+if __name__ == "__main__":
+  path = "energy/nov_data.tsf"
+  d = DataManager(path)
+  for k,v in d.train.items():
+    print(len(v))
