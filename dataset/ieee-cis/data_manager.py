0162fae60e5b3e0b305bb407e447dcbbef511f8d
hung <bahung1tt@gmail.com>
2022-07-25 14:50:57 +1000

    Update gitignore for venv stuffs Remove magic number in data_manager.py

--- dataset/ieee-cis/data_manager.py
+++ dataset/ieee-cis/data_manager.py
@@ -2,25 +2,27 @@ from tsf_loader import convert_tsf_to_dataframe
 import datetime
 
 class DataManager:
-  def __init__(self, tsf_file_path):
-    tsf = convert_tsf_to_dataframe(path)
+  def __init__(self, 
+               tsf_file_path,
+               # Default phase1 and phase2 time in the documents
+               phase1_time = datetime.datetime(day=1, month=10, year=2020, hour=0, minute=0, second=0),
+               phase2_time = datetime.datetime(day=1, month=11, year=2020, hour=0, minute=0, second=0)):
 
+    tsf = convert_tsf_to_dataframe(path)
     self.df = tsf[0]
 
     series_name = self.df["series_name"]
     series_value = self.df["series_value"]
     start_timestamp = self.df["start_timestamp"]
 
-    phase_1_time = datetime.datetime(day=1, month=10, year=2020, hour=0, minute=0, second=0)
-    phase_2_time = datetime.datetime(day=1, month=11, year=2020, hour=0, minute=0, second=0)
 
     self.train = dict()
     self.phase1 = dict()
     self.phase2 = dict()
 
     for name, value, stime in zip(series_name, series_value, start_timestamp):
-      phase_1_idx = int((phase_1_time - stime).total_seconds()/60/15)
-      phase_2_idx = int((phase_2_time - stime).total_seconds()/60/15)
+      phase_1_idx = int((phase1_time - stime).total_seconds()/60/15)
+      phase_2_idx = int((phase2_time - stime).total_seconds()/60/15)
 
       self.train[name] = value[:phase_1_idx]
       self.phase1[name] = value[phase_1_idx:phase_2_idx]
