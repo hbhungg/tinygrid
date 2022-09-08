9d5c518907ec03d1b14d4ad3d94550697474d283
HarrisAsync <42229360+HarrisAsync@users.noreply.github.com>
2022-09-08 17:51:34 +1000

    website changes

---
+++ website/vega_lite_script.js
@@ -0,0 +1,32 @@
+// Vega lite graphs
+const jsonData = {
+  "data": {
+    "width": "container",
+    "height": "container",
+    "sequence": {
+      "start": 0,
+      "stop": 12.7,
+      "step": 0.1,
+      "as": "x"
+    }
+  },
+  "transform": [
+    {
+      "calculate": "sin(datum.x)",
+      "as": "sin(x)"
+    }
+  ],
+  "mark": "line",
+  "encoding": {
+    "x": {
+      "field": "x",
+      "type": "quantitative"
+    },
+    "y": {
+      "field": "sin(x)",
+      "type": "quantitative"
+    }
+  }
+};
+vegaEmbed('#vis1', jsonData);
+vegaEmbed('#vis2', jsonData);
