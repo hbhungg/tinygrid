46af5ec1e644e783a7e996e6846ac7f524ebb7aa
HarrisAsync <42229360+HarrisAsync@users.noreply.github.com>
2022-09-08 11:09:19 +1000

    Finished scroll feature
    Mouse event scroll and javascript scroll are incomabitible after much research. Side bar heading will not be clickable.

--- Website/script.js
+++ Website/script.js
@@ -27,41 +27,33 @@ function left_btn_selected_helper(button_num) {
 }
 
 function left_btn_selected(button_num) {
-  // Check to do nothing
-  if (button_num == current_left_btn_selected) {
-    return;
-  }
-  // Send viewport to element height
+  /*
   var content_selected = document.getElementById("content_" + button_num.toString());
-  window.scrollTo({
-    top: window.pageYOffset + content_selected.getBoundingClientRect().top,
-    behavior: 'smooth'
-  });
+  // Send viewport to element height
+  window.scrollTo({top: window.pageYOffset + content_selected.getBoundingClientRect().top});
+
+  // Change left button style
   left_btn_selected_helper(button_num);
+  */
 }
 
-/*
-window.addEventListener("scroll", function() {
-  const current_element = document.getElementById("content_" + current_left_btn_selected.toString());
-  const above_element = document.getElementById("content_" + (current_left_btn_selected - 1).toString());
-  const bellow_element = document.getElementById("content_" + (current_left_btn_selected + 1).toString());
-  
-  if (bellow_element != null) {
-    if (window.scrollY > (bellow_element.offsetTop + bellow_element.offsetHeight - bellow_element.clientHeight) && window.scrollY > (current_element.offsetTop + current_element.offsetHeight)) {
-      left_btn_selected_helper(current_left_btn_selected + 1);
+function scroll_content_effect() {
+  var lst_ids = [1,2,3,4,5];
+  for (let i=0; i<lst_ids.length; i++) {
+    var el = document.getElementById("content_" + lst_ids[i].toString());
+    if (window.scrollY <= el.offsetTop + el.offsetHeight && window.scrollY >= window.pageYOffset + el.getBoundingClientRect().top) {
+      left_btn_selected_helper(lst_ids[i]);
     }
-  }
-  if (above_element != null) {
-    if (window.scrollY < (above_element.offsetTop + above_element.offsetHeight - above_element.clientHeight) && window.scrollY < (current_element.offsetTop + current_element.offsetHeight)) {
-      left_btn_selected_helper(current_left_btn_selected - 1);
-    }
-  }
-});
-*/
+  };
 
+}
+
+// Window event listeners
+window.addEventListener("scroll", function() {scroll_content_effect();}, {passive: true});
 window.addEventListener("load", function() {
   window.scrollTo({
     top: 0,
     behavior: 'smooth'
   });
 });
+
