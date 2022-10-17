fe899ef8be36d909fbf31d62d7535d9d93ebbb13
hung <bahung1tt@gmail.com>
2022-10-17 12:57:56 +1100

    update test for mase

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -3,8 +3,8 @@ import datetime
 import numpy as np
 import math
 
-from tinygrid.utils import mase
-from tinygrid.dataloader import DataCleaner
+from tinygrid.utils import mase, Const
+from tinygrid.dataset import IEEE_CISMixin
 
 class TestMetrics(unittest.TestCase):
   def test_mase_type_checking(self):
@@ -18,22 +18,33 @@ class TestMetrics(unittest.TestCase):
 
   # TODO: Compare mase output with the sample_submission.csv
   def test_mase(self):
-    with open("sample_mase_data.txt") as f:
-      sample_ans = [i.split() for i in f.readlines()]
-    sample_ans = [float(i[2]) for i in sample_ans[:-2]]
+    # These values are generated from the given mase calc from the competition.
+    sample_ans = {
+        "Building0":3.111075,  
+        "Building1":4.589733, 
+        "Building3":4.1218123,
+        "Building4":2.966333, 
+        "Building5":1.720494,
+        "Building6":10.558116,
+        "Solar0":   3.355351,
+        "Solar1":   1.543332,
+        "Solar2":   1.748193,
+        "Solar3":   2.043440,
+        "Solar4":   1.851925,
+        "Solar5":   2.839265}
 
-    start = datetime.datetime(day=31, month=8, year=2020, hour=23, minute=59, second=59)
-    end = datetime.datetime(day=30, month=9, year=2020, hour=23, minute=59, second=59)
-    du = DataCleaner()
-    cols = du.cols
+    start = datetime.datetime(day=31, month=8, year=2020, hour=23, minute=59, second=59, tzinfo=Const.UTC)
+    end = datetime.datetime(day=30, month=9, year=2020, hour=23, minute=59, second=59, tzinfo=Const.UTC)
     diff = 0.00001
+    energy = IEEE_CISMixin._load_energy_data()
 
     pred = np.zeros(2880)
-    for col, sa in zip(cols, sample_ans):
-      data, _ = du.load_data_helper(col)
+    for col in energy.keys():
+      data = energy[col]
       training = np.array(data[:start]['energy'].values)
       true = np.array(data[start:end]['energy'].values)
       r_mase = mase(pred, true, training)
+      sa = sample_ans[col]
 
       with self.subTest(msg=col):
         self.assertTrue(math.isclose(r_mase, sa, rel_tol=diff), 
