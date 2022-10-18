c87e53c20381c496bb016159dd0d921921259cae
HarrisAsync <42229360+HarrisAsync@users.noreply.github.com>
2022-10-18 18:08:47 +1100

    updates to tests

--- test/test_ieee_cis.py
+++ test/test_ieee_cis.py
@@ -4,21 +4,15 @@ from tinygrid.dataset import IEEE_CISMixin
 class load_ERA5_weather_data_tests(unittest.TestCase):
   def setUp(self):
     self.cl = IEEE_CISMixin
-  def tearDown(self):
-    pass
-
   def test_call(self):
     try:
       self.cl._load_ERA5_weather_data()
     except:
       self.fail("Instance failed at _load_ERA5_weather_data")
 
-
 class load_AEMO_price_datatests(unittest.TestCase):
   def setUp(self):
     self.cl = IEEE_CISMixin
-  def tearDown(self):
-    pass
   def test_call(self):
     try:
       self.cl._load_AEMO_price_data()
@@ -28,14 +22,29 @@ class load_AEMO_price_datatests(unittest.TestCase):
 class load_energy_data_tests(unittest.TestCase):
   def setUp(self):
     self.cl = IEEE_CISMixin
-  def tearDown(self):
-    pass  
   def test_call(self):
     try:
       self.cl._load_energy_data()
     except:
       self.fail("Instance failed at _load_energy_data")
 
+class load_instance_data(unittest.TestCase):
+  def setUp(self):
+    self.cl = IEEE_CISMixin
+  def test_call(self):
+    try:
+      self.cl._load_instance_data()
+    except:
+      self.fail("Instance failed at _load_instance_data")
+
+class load_instance_sample_solution_data(unittest.TestCase):
+  def setUp(self):
+    self.cl = IEEE_CISMixin
+  def test_call(self):
+    try:
+      self.cl._load_instance_sample_solution_data()
+    except:
+      self.fail("Instance failed at _load_instance_sample_solution_data")
 
 if __name__ == '__main__':
     unittest.main()
